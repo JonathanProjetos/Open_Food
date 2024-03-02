90ffbd8702c75af15a74789c645c56a44b18cfdf
Jonathan Santos <jonathan01347@gmail.com>
2024-03-02 19:18:55 -0300

    feat(index) Adiciona módulo de consulta de produtos

---
+++ src/products/utils/query/index.ts
@@ -0,0 +1,31 @@
+import { fieldIngredientsAndListIngredients } from './fieldIngredientsAndListIngredients';
+import { filedNutritionAssessmentNote } from './filedNutritionAssessmentNote';
+import { arrayOfCalorieRatingTierList } from './fieldValues';
+import { filedServingSize } from './filedServingSize';
+import { fieldCaloricValueTable } from './fieldCaloricValueTable';
+import { fieldQuantity } from './fieldQuantity';
+import { fieldNovaScore } from './fieldNovaScore';
+import { fieldTextNova } from './fieldTextNova';
+import { fieldTitle } from './fieldTitle';
+import { filterPageLinksById } from './filterPageLinksById';
+import { fieldId } from './fieldId';
+import { fieldTextScore } from './fieldTextScore';
+import { linkArray } from './linkArray';
+
+const funcsProductDetail = {
+  fieldIngredientsAndListIngredients,
+  filedNutritionAssessmentNote,
+  arrayOfCalorieRatingTierList,
+  filedServingSize,
+  fieldCaloricValueTable,
+  fieldQuantity,
+  fieldNovaScore,
+  fieldTextNova,
+  fieldTitle,
+  filterPageLinksById,
+  fieldId,
+  fieldTextScore,
+  linkArray,
+};
+
+export default funcsProductDetail;
