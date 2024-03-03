5ac5afb50def1cc5a24b5eaa51c7a7521acd9a82
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:55:01 -0300

    feat(filedNutritionAssessmentNote) Adiciona comentários.

--- src/products/utils/query/filedNutritionAssessmentNote.ts
+++ src/products/utils/query/filedNutritionAssessmentNote.ts
@@ -1,26 +1,27 @@
 import { Page } from 'puppeteer';
 
+// ------- Busco a nota de avaliação nutricional do produto -------
 export const filedNutritionAssessmentNote = async (page: Page, id: string) => {
   try {
+    // ------- Busco pelo elemento antes de executar -------
     const element = page.$('#attributes_grid li a div .attr_text h4');
 
     if (element) {
-      const textNutrition = await page.$eval(
+      const textNutrition = await page?.$eval(
         '#attributes_grid li a div .attr_text h4',
-        (el) => el.innerHTML.trim(),
+        (el) => el?.innerHTML?.trim(),
       );
 
-      const formatedtextNutrition =
-        textNutrition && textNutrition.split(' ')[1];
+      const formatedtextNutrition = textNutrition?.split(' ')[1];
 
       return formatedtextNutrition;
     }
+    // Se não encontrar o elemento e não gerar o error retorno um valor default '?'
     return '?';
   } catch (e: any) {
     console.error(e);
-    await page.goto(`https://br.openfoodfacts.org/${id}`, {
-      waitUntil: 'load',
-    });
+    // Em caso de erro devido à ausência de dados, tento acessar a página novamente para continuar a raspagem dos dados faltantes.
+    await page.goto(`https://br.openfoodfacts.org/${id}`);
     return '?';
   }
 };
