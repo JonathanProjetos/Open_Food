a81c7fa9b883a98201b77ee55d92bc01d5110973
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:49:47 -0300

    fix(fieldTextNova) Adiciona tratamento de erro e mensagem padrão para campo de texto Nova

--- src/products/utils/query/fieldTextNova.ts
+++ src/products/utils/query/fieldTextNova.ts
@@ -1,26 +1,28 @@
 import { Page } from 'puppeteer';
 
+// ------- Busco o Texto do campo nova do produto -------
 export const fieldTextNova = async (page: Page, id: string) => {
   try {
     const element = page.$(
       '#attributes_grid li:nth-last-child(2) a div .attr_text span',
     );
 
+    // ------- Busco pelo elemento antes de executar -------
     if (element) {
-      const novaTitle = await page.$eval(
+      const novaTitle = await page?.$eval(
         '#attributes_grid li:nth-last-child(2) a div .attr_text span',
-        (el) => el.innerHTML.trim(),
+        (el) => el?.innerHTML.trim(),
       );
-      const formatedNovaTitle = novaTitle.split(' ')[1];
+      const formatedNovaTitle = novaTitle?.split(' ')[1];
       return formatedNovaTitle;
     }
-
+    // Se não encontrar o elemento e não gerar o error retorno um valor default '?'
     return '?';
   } catch (error) {
     console.log(error);
-    await page.goto(`https://br.openfoodfacts.org/${id}`, {
-      waitUntil: 'load',
-    });
+    // Em caso de erro devido à ausência de dados, tento acessar a página novamente para continuar a raspagem dos dados faltantes.
+    await page.goto(`https://br.openfoodfacts.org/${id}`);
+    // Em caso de erro devido à ausência de dados forneço um valor default '?'
     return '?';
   }
 };
