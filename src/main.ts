cfb5877be085a82c2a61150dee10cbe03c5cfd89
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 14:16:23 -0300

    fix(main) Atualiza mensagem de log para exibir a porta correta do servidor

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${PORT}`);
   });
 }
 bootstrap();
