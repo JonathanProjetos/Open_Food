049a1dcb464f77835e009a84a24ec35acf186df3
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:48:07 -0300

    refactor(fieldIngredientsAndListIngredients) Refatorar função fieldIngredientsAndListIngredients

--- src/products/utils/query/fieldIngredientsAndListIngredients.ts
+++ src/products/utils/query/fieldIngredientsAndListIngredients.ts
@@ -1,63 +1,62 @@
 import { Page } from 'puppeteer';
 
+// ------- Busco os ingredientes e a lista de ingredientes -------
 export const fieldIngredientsAndListIngredients = async (
   page: Page,
   id: string,
 ) => {
   try {
+    // ------- Busco pelo elemento antes de executar -------
     const element = page.$(
       '#panel_ingredients_analysis_content > ul:nth-last-child(4)  h4',
     );
 
     if (element) {
-      const hasPalmOil =
-        (await page?.$eval(
-          '#panel_ingredients_analysis_content > ul:nth-last-child(4)  h4',
-          (el) => el?.textContent?.trim(),
-        )) ?? 'unknown';
+      const hasPalmOil = await page?.$eval(
+        '#panel_ingredients_analysis_content > ul:nth-last-child(4)  h4',
+        (el) => el?.textContent.trim(),
+      );
 
-      const vegan =
-        (await page?.$eval(
-          '#panel_ingredients_analysis_content > ul:nth-last-child(3)  h4',
-          (el) => el?.className?.includes('good'),
-        )) ?? 'unknown';
+      const vegan = await page?.$eval(
+        '#panel_ingredients_analysis_content > ul:nth-last-child(3)  h4',
+        (el) => el?.className.includes('good'),
+      );
 
-      const vegetarian =
-        (await page?.$eval(
-          '#panel_ingredients_analysis_content > ul:nth-last-child(2)  h4',
-          (el) => el?.className?.includes('good'),
-        )) ?? 'unknown';
+      const vegetarian = await page?.$eval(
+        '#panel_ingredients_analysis_content > ul:nth-last-child(2)  h4',
+        (el) => el?.className.includes('good'),
+      );
 
       // ------- Pegando a lista de ingredientes -------
 
       const ingredientsList = await page?.$eval('.panel_text', (el) =>
-        el?.textContent?.replace(/\s+|\n+/g, ' ').split(','),
+        el?.textContent.replace(/\s+|\n+/g, ' ').split(','),
       );
 
       // -------- Criando o objeto de ingredientes -------
 
       const ingredients = {
-        hasPalmOil: hasPalmOil && hasPalmOil,
-        isVegan: vegan && vegan,
-        isVegetarian: vegetarian && vegetarian,
+        hasPalmOil: hasPalmOil,
+        isVegan: vegan,
+        isVegetarian: vegetarian,
         list: ingredientsList,
       };
 
       return ingredients;
     }
+    // Se não encontrar o elemento e não gerar o error retorno um valor default '{}'
     return {};
   } catch (e: any) {
     console.error(e);
-
+    // Retornoo um objeto com valores default '?' em caso de erro
     const ingredients = {
       hasPalmOil: '?',
       isVegan: '?',
       isVegetarian: '?',
       list: '?',
     };
-    await page.goto(`https://br.openfoodfacts.org/${id}`, {
-      waitUntil: 'load',
-    });
+    // Em caso de erro devido à ausência de dados, tento acessar a página novamente para continuar a raspagem dos dados faltantes.
+    await page.goto(`https://br.openfoodfacts.org/${id}`);
     return ingredients;
   }
 };
