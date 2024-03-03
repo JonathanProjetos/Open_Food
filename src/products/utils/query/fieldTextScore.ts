09364309f3e184a29a5510cbc87a617e0b0fc640
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:50:09 -0300

    fix(fieldTextScore) Adiciona tratamento para elemento ausente em fieldTextScore.ts

--- src/products/utils/query/fieldTextScore.ts
+++ src/products/utils/query/fieldTextScore.ts
@@ -1,14 +1,23 @@
 import { Page } from 'puppeteer';
 
+// ------- Busco o score do produto -------
 export const fieldTextScore = async (page: Page) => {
   try {
-    const nutritionTitle = await page.$eval(
-      '#attributes_grid li a div .attr_text span',
-      (el) => el.innerHTML.trim(),
-    );
-    return nutritionTitle;
+    // ------- Busco pelo elemento antes de executar -------
+    const element = page.$('#attributes_grid li a div .attr_text span');
+
+    if (element) {
+      const nutritionTitle = await page?.$eval(
+        '#attributes_grid li a div .attr_text span',
+        (el) => el?.innerHTML.trim(),
+      );
+      return nutritionTitle;
+    }
+    // Se não encontrar o elemento e não gerar o error retorno um valor default '?'
+    return '?';
   } catch (error) {
     console.error(error);
+    // Em caso de erro devido à ausência de dados forneço um valor default '?'
     return '?';
   }
 };
