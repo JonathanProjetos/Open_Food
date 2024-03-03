30998d5861b8802de43c40b2c6aa2a051597e74b
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 14:15:14 -0300

    feat(dotenv) Adiciona dependência dotenv e configuração de porta

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const PORT = process.env.PORT || 3001;
 
   const config = new DocumentBuilder()
     .setTitle('Open_Food API')
@@ -16,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001, () => {
+  await app.listen(PORT, () => {
     console.log('Server is running on port 3000');
   });
 }
