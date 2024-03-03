a902265d6746e51568208007d590e8085e583175
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:57:16 -0300

    feat(linkArray) Adiciona comentários.

--- src/products/utils/query/linkArray.ts
+++ src/products/utils/query/linkArray.ts
@@ -1,17 +1,21 @@
 import { Page } from 'puppeteer';
 
+// ------- Busco os links da página -------
 export const linkArray = async (page: Page) => {
   try {
+    // ------- Busco pelo elemento antes de executar -------
     const element = await page.$('#search_results div div ul li a');
 
     if (element) {
-      const links = await page.$$eval('#search_results div div ul li a', (el) =>
-        el.map((a) => a.href),
+      const links = await page?.$$eval(
+        '#search_results div div ul li a',
+        (el) => el?.map((a) => a?.href),
       );
       return links;
     }
   } catch (error) {
     console.log(error);
+    // Em caso de erro devido à ausência de dados forneço um array vazio.
     return [];
   }
 };
