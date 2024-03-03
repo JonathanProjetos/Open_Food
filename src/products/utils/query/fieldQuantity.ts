2d04e36112a886f3baf487f3c5318df7d0e93d6b
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:49:10 -0300

    fix(fieldQuantity) Adiciona tratamento de erro e comentários no arquivo fieldQuantity.ts

--- src/products/utils/query/fieldQuantity.ts
+++ src/products/utils/query/fieldQuantity.ts
@@ -1,24 +1,26 @@
 import { Page } from 'puppeteer';
 import { decode } from 'he';
 
+// ------- Busco a quantidade do produto -------
 export const fieldQuantity = async (page: Page, id: string) => {
   try {
+    // ------- Busco pelo elemento antes de executar -------
     const element = page.$('#field_quantity > #field_quantity_value');
 
     if (element) {
-      const quantity = await page.$eval(
+      const quantity = await page?.$eval(
         '#field_quantity > #field_quantity_value',
-        (el) => el.innerHTML.trim(),
+        (el) => el?.innerHTML.trim(),
       );
       const quantityTextDecoded = decode(quantity);
       return quantityTextDecoded;
     }
+    // Se não encontrar o elemento e não gerar o error retorno um valor default '?'
     return '?';
   } catch (error) {
     console.error(error);
-    await page.goto(`https://br.openfoodfacts.org/${id}`, {
-      waitUntil: 'load',
-    });
+    // Em caso de erro devido à ausência de dados, tento acessar a página novamente para continuar a raspagem dos dados faltantes.
+    await page.goto(`https://br.openfoodfacts.org/${id}`);
     return '?';
   }
 };
