339f14b2e54ffa6a8f6ddeaed18a08b222d55fc5
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:50:34 -0300

    feat(fieldTitle) Adiciona tratamento de erro e comentários no campo de título

--- src/products/utils/query/fieldTitle.ts
+++ src/products/utils/query/fieldTitle.ts
@@ -1,22 +1,25 @@
 import { Page } from 'puppeteer';
 import { decode } from 'he';
 
+// ------- Busco o título do produto -------
+// Faço uso da função decode para decodificar os caracteres especiais.
+
 export const fieldTitle = async (page: Page, id: string) => {
   try {
+    // ------- Busco pelo elemento antes de executar -------
     const element = page.$('.title-1');
 
     if (element) {
-      const title = await page.$eval('.title-1', (el) => el.innerHTML.trim());
+      const title = await page?.$eval('.title-1', (el) => el?.innerHTML.trim());
       const titleDecoded = decode(title);
       return titleDecoded;
     }
-
+    // Se não encontrar o elemento e não gerar o error retorno um valor default '?'
     return '?';
   } catch (error) {
     console.error(error);
-    await page.goto(`https://br.openfoodfacts.org/${id}`, {
-      waitUntil: 'load',
-    });
+    // Em caso de erro devido à ausência de dados, tento acessar a página novamente para continuar a raspagem dos dados faltantes.
+    await page.goto(`https://br.openfoodfacts.org/${id}`);
     return '?';
   }
 };
