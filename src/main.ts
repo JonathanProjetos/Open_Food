4c1ffb85cdb86ff5ef085ba88bbcb6cbf0e6b280
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:15:09 -0300

    feat(main)Atualizações na descrição da API Open_Food

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Scraping API')
-    .setDescription('Raspagem de dados de produtos alimentícios.')
+    .setTitle('Open_Food API')
+    .setDescription(
+      'A API Open_Food é uma aplicação que coleta dados do site OpenFoodsFacts. Por meio de seus endpoints, é possível filtrar os dados por nível de nutrição, variando de A a E, e sua pontuação, que varia de 1 a 5. Se nenhum parâmetro for fornecido, todos os dados serão retornados.',
+    )
     .setVersion('1.0')
     .addTag('Products')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3008, () => {
+  await app.listen(3001, () => {
     console.log('Server is running on port 3000');
   });
 }
