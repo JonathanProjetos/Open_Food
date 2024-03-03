705641ffbf355071d3b88d5c8ab899c1298956bb
Jonathan Santos <jonathan01347@gmail.com>
2024-03-03 12:56:33 -0300

    feat(filedPageLinksById) Adiciona comentários.

--- src/products/utils/query/filterPageLinksById.ts
+++ src/products/utils/query/filterPageLinksById.ts
@@ -1,17 +1,20 @@
 import { Page } from 'puppeteer';
 
+// ------- Busco os links da página e filtro pelo id fornecido -------
 export const filterPageLinksById = async (page: Page, id: string) => {
   try {
+    // ------- Busco pelo elemento antes de executar -------
     const element = page.$('#search_results div div ul li a');
 
     if (element) {
-      const productsLinks = await page.$$eval(
+      const productsLinks = await page?.$$eval(
         '#search_results div div ul li a',
-        (el) => el.map((a) => a.href),
+        (el) => el?.map((a) => a?.href),
       );
-      const link = productsLinks.filter((p) => p.includes(id))[0];
+      const link = productsLinks?.filter((p) => p?.includes(id))[0];
       return link;
     }
+    // Se não encontrar o elemento e não gerar o error retorno um valor default '?'
     return '?';
   } catch (error) {
     console.error(error);
